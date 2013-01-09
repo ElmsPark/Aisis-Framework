9a70c792af80b8bbb1c88c5ef5cdcbac63d036ff
Adam <a-kyle@hotmail.com>
2013-01-08 20:24:33 -0700

    Added hover overs for information in the admin panel, added more form fields. everything is working as it should.

--- CoreTheme/AdminPanel/Form/SiteDesign.php
+++ CoreTheme/AdminPanel/Form/SiteDesign.php
@@ -12,6 +12,14 @@ class CoreTheme_AdminPanel_Form_SiteDesign extends CoreTheme_Form_Form{
 			$this->_radio_rows_element(),
 			$this->_radio_list_element(),
 			$this->_radio_no_posts_element(),
+			$this->_sidebar_header_content(),
+			$this->_checkbox_no_page_sidebar(),
+			$this->_checkbox_no_posts_sidebar(),
+			$this->_mini_feed_config(),
+			$this->_checkbox_disable_mini_feed(),
+			$this->_checkbox_disable_mini_feed_from_front(),
+			$this->_checkbox_display_mini_feed_pages(),
+			$this->_checkbox_display_mini_feed_templates(),
 			$this->_submit_element()	
 		);
 		
@@ -21,7 +29,7 @@ class CoreTheme_AdminPanel_Form_SiteDesign extends CoreTheme_Form_Form{
 	protected function _create_header_content(){
 
 		$content_array = array(
-			'class' => 'well headLine',
+			'class' => 'well headLine wellChangePushDown',
 			'content' => '<h1>Core Look</h1><p>Choose from the option bellow to decide 
 				how your theme will look to others!</p>',
 		);
@@ -38,8 +46,11 @@ class CoreTheme_AdminPanel_Form_SiteDesign extends CoreTheme_Form_Form{
 			'value' => 'display_rows',
 			'class' => 'display',
 			'id' => 'rows',
-			'label' => ' Display posts as rows. <a href="#radioRows" data-toggle="modal">
-				<i class="icon-info-sign"> </i></a>',
+			'label' => ' Display posts as rows.
+			<a href="#" id="display_rows" rel="popover" 
+			data-content="example content" 
+			data-trigger="hover"
+			data-original-title="example"><i class="icon-info-sign"></i></a>',
 			'checked' => $this->set_element_checked('display_rows', 'aisis_core', 'display_rows')		
 		);
 		
@@ -54,8 +65,11 @@ class CoreTheme_AdminPanel_Form_SiteDesign extends CoreTheme_Form_Form{
 			'name' => 'aisis_core[display_rows]',
 			'value' => 'list',
 			'class' => 'display',
-			'label' => ' Display posts a list. <a href="#radioLists" data-toggle="modal">
-			<i class="icon-info-sign"> </i></a>',
+			'label' => ' Display posts a list.
+			<a href="#" id="list" rel="popover" 
+			data-content="example content" 
+			data-trigger="hover"
+			data-original-title="example"><i class="icon-info-sign"></i></a>',
 			'checked' => $this->set_element_checked('list', 'aisis_core', 'display_rows')
 		);
 	
@@ -78,6 +92,126 @@ class CoreTheme_AdminPanel_Form_SiteDesign extends CoreTheme_Form_Form{
 		$radio = new CoreTheme_Form_Elements_Radio($radio_element, $this->_sub_section_no_posts_array());
 		
 		return $radio;
+	}
+	
+	protected function _sidebar_header_content(){
+
+		$content_array = array(
+			'class' => 'well headLine wellChangePushDown',
+			'content' => '<h1>Sidebar Configuation</h1><p>Choose from bellow to <strong>NOT</strong> show side
+			bars on specific parts of the blog.</p>
+			<p class="text-info">Some page templates do not contain a sidebar. 
+			How ever the default page template does.</p>',
+		);
+		
+		$header_content = new AisisCore_Form_Elements_ContentElement($content_array);
+		
+		return $header_content;
+	}
+	
+	protected function _checkbox_no_page_sidebar(){
+		$checkbox_element = array(
+			'name' => 'aisis_core[no_sidebar_page]',
+			'value' => 'no_sidebar_page',
+			'label' => ' Do <strong>not</strong> display a side bar on pages.',
+			'checked' => $this->set_element_checked('no_sidebar_page', 'aisis_core', 'no_sidebar_page')	
+		);
+	
+		$checkbox = new CoreTheme_Form_Elements_Checkbox($checkbox_element);
+		
+		return $checkbox;		
+	}
+	
+	protected function _checkbox_no_posts_sidebar(){
+		$checkbox_element = array(
+			'name' => 'aisis_core[no_sidebar_post]',
+			'value' => 'no_sidebar_post',
+			'label' => ' Do <strong>not</strong> display a side bar on posts.',
+			'checked' => $this->set_element_checked('no_sidebar_post', 'aisis_core', 'no_sidebar_page')	
+		);
+	
+		$checkbox = new CoreTheme_Form_Elements_Checkbox($checkbox_element);
+		
+		return $checkbox;		
+	}
+	
+	protected function _mini_feed_config(){
+
+		$content_array = array(
+			'class' => 'well headLine wellChangePushDown',
+			'content' => '<h1>Mini Feed Configuration</h1>
+			<p>Choose where the mini feed appears</p>
+			<p class="text-info">Some changes are global and will whipe out the ability
+			to choose other options.</p>',
+		);
+		
+		$header_content = new AisisCore_Form_Elements_ContentElement($content_array);
+		
+		return $header_content;
+	}
+	
+	protected function _checkbox_disable_mini_feed(){
+		$checkbox_element = array(
+			'name' => 'aisis_core[no_mini_feed]',
+			'value' => 'no_mini_feed',
+			'label' => ' Disable the minifeed from the site.',
+			'checked' => $this->set_element_checked('no_mini_feed', 'aisis_core', 'no_mini_feed')	
+		);
+	
+		$checkbox = new CoreTheme_Form_Elements_Checkbox($checkbox_element);
+		
+		return $checkbox;		
+	}
+	
+	protected function _checkbox_disable_mini_feed_from_front(){
+		$checkbox_element = array(
+			'name' => 'aisis_core[no_mini_feed]',
+			'value' => 'no_mini_front',
+			'label' => ' Disable mini feed from the front page of posts. 
+			<a href="#" id="no_mini_feed_front" rel="popover" 
+			data-content="example content" 
+			data-trigger="hover"
+			data-original-title="example"><i class="icon-info-sign"></i></a>',
+			'checked' => $this->set_element_checked('no_mini_front', 'aisis_core', 'no_mini_front')	
+		);
+	
+		$checkbox = new CoreTheme_Form_Elements_Checkbox($checkbox_element);
+		
+		return $checkbox;		
+	}
+	
+	protected function _checkbox_display_mini_feed_pages(){
+		$checkbox_element = array(
+			'name' => 'aisis_core[disaply_mini_feed_pages]',
+			'value' => 'disaply_mini_feed_pages',
+			'label' => ' Display mini feeds on pages. 
+			<a href="#" id="display_mini_feed_pages" rel="popover" 
+			data-content="example content" 
+			data-trigger="hover"
+			data-original-title="example"><i class="icon-info-sign"></i></a>',
+			'checked' => $this->set_element_checked('disaply_mini_feed_pages', 'aisis_core', 'disaply_mini_feed_pages')	
+		);
+	
+		$checkbox = new CoreTheme_Form_Elements_Checkbox($checkbox_element);
+		
+		return $checkbox;		
+	}
+	
+	protected function _checkbox_display_mini_feed_templates(){
+		$checkbox_element = array(
+			'name' => 'aisis_core[disaply_mini_feed_templates]',
+			'value' => 'disaply_mini_feed_templates',
+			'label' => ' Display mini feeds on templates. 
+			<a href="#" id="display_templates" rel="popover" 
+			data-content="example content" 
+			data-trigger="hover"
+			data-original-title="example"><i class="icon-info-sign"></i></a>',
+			'checked' => $this->set_element_checked('disaply_mini_feed_templates', 'aisis_core', 'disaply_mini_feed_templates')	
+		);
+	
+		$checkbox = new CoreTheme_Form_Elements_Checkbox($checkbox_element);
+		
+		return $checkbox;		
 	}
 	
 	protected function sub_section_rows_array(){
