61080bc552753d40e4aff12746f3b2fd3d8b8966
Adam <a-kyle@hotmail.com>
2013-01-13 12:38:25 -0700

    We now have a fully working comment section which also support pagination

--- comments.php
+++ comments.php
@@ -1,91 +1,116 @@
 <?php
- 
-// Do not delete these lines
-if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
-die ('Please do not load this page directly. Thanks!');
- 
-if ( post_password_required() ) { ?>
-<p class="nocomments">This post is password protected. Enter the password to view comments.</p>
+if(post_password_required()){ ?>
+<p class="nocomments">
+	This post is password protected. Enter the password to view comments.
+</p>
 <?php
-return;
+	return;
 }
-?>
- 
-<!-- You can start editing here. -->
- 
-<?php if ( have_comments() ) : ?>
-<h3 id="comments"><?php comments_number('No Responses', 'One Response', '% Responses' );?> to &#8220;<?php the_title(); ?>&#8221;</h3>
- 
-<div class="navigation">
-<div class="alignleft"><?php previous_comments_link() ?></div>
-<div class="alignright"><?php next_comments_link() ?></div>
-</div>
+if(have_comments()):?>
+<h3 id="comments">
+	<?php comments_number('No Responses', 'One Response', '% Responses' );?> 
+	to &#8220;<?php the_title(); ?>&#8221;
+</h3>
  
 <ol class="commentlist">
-<?php wp_list_comments(); ?>
+	<?php wp_list_comments(); ?>
 </ol>
- 
-<div class="navigation">
-<div class="alignleft"><?php previous_comments_link() ?></div>
-<div class="alignright"><?php next_comments_link() ?></div>
-</div>
-<?php else : // this is displayed if there are no comments so far ?>
- 
-<?php if ('open' == $post->comment_status) : ?>
-<!-- If comments are open, but there are no comments. -->
- 
-<?php else : // comments are closed ?>
-<!-- If comments are closed. -->
+
+<ul class="pager paddingBottom20">
+	<li class="previous">
+		<?php previous_comments_link() ?>
+	</li>
+	<li class="next">
+	  	<?php next_comments_link() ?>
+	</li>
+</ul> 
+
+<?php else : 
+	if ('open' == $post->comment_status) : 
+	else : ?>
 <p class="nocomments">Comments are closed.</p>
  
-<?php endif; ?>
+	<?php endif; ?>
 <?php endif; ?>
  
 <?php if ('open' == $post->comment_status) : ?>
  
 <div id="respond">
- 
-<h3><?php comment_form_title( 'Leave a Reply', 'Leave a Reply to %s' ); ?></h3>
- 
-<div class="cancel-comment-reply">
-<small><?php cancel_comment_reply_link(); ?></small>
-</div>
- 
+	<h3><?php comment_form_title( 'Leave a Reply', 'Leave a Reply to %s' ); ?></h3>
+	<div class="cancel-comment-reply">
+		<small><?php cancel_comment_reply_link(); ?></small>
+	</div>	 
 <?php if ( get_option('comment_registration') && !$user_ID ) : ?>
-<p>You must be <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?redirect_to=<?php echo urlencode(get_permalink()); ?>">logged in</a> to post a comment.</p>
+<p>You must be 
+	<a href="<?php echo get_option('siteurl'); ?>/wp-login.php?redirect_to=
+	<?php echo urlencode(get_permalink()); ?>">logged in</a> 
+	to post a comment.
+</p>
 <?php else : ?>
  
-<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
- 
+	<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" 
+		method="post" id="commentform" class="form-vertical">
 <?php if ( $user_ID ) : ?>
- 
-<p>Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="Log out of this account">Log out &raquo;</a></p>
- 
+		<p class="text-info">
+			Logged in as 
+			<a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php">
+				<?php echo $user_identity; ?>
+			</a>. 
+			<a href="<?php echo wp_logout_url(get_permalink()); ?>" 
+			title="Log out of this account">
+				Log out &raquo;
+			</a>
+		</p>
 <?php else : ?>
  
-<p><input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="22" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
-<label for="author"><small>Name <?php if ($req) echo "(required)"; ?></small></label></p>
- 
-<p><input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="22" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
-<label for="email"><small>Mail (will not be published) <?php if ($req) echo "(required)"; ?></small></label></p>
- 
-<p><input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="22" tabindex="3" />
-<label for="url"><small>Website</small></label></p>
+		<div class="control-group">
+			<label for="author" class="control-label">
+				<small>Name <?php if ($req) echo "(required)"; ?></small>
+			</label>
+			<div class="controls">
+				<input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" 
+				size="22" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
+			</div>
+		</div>
+		 
+		<div class="control-group">
+			<label for="email" class="control-label">
+				<small>Mail (will not be published) <?php if ($req) echo "(required)"; ?></small>
+			</label>
+			<div class="controls">
+				<input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" 
+				size="22" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
+			</div>
+		</div>
+		 
+		<div class="control-label">
+			<label for="url" class="control-label">
+				<small>Website</small>
+			</label>
+			<div class="controls">
+				<input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" 
+					size="22" tabindex="3" />
+			</div>
+		</div>
  
 <?php endif; ?>
  
-<!--<p><small><strong>XHTML:</strong> You can use these tags: <code><?php echo allowed_tags(); ?></code></small></p>-->
+		<div class="control-group marginTop20">
+			<div class="controls">
+				<label class="control-label"><strong>Your comment!</strong></label>
+				<textarea name="comment" id="comment" cols="100%" rows="10" tabindex="4"></textarea>
+			</div>
+		</div>
+		
+		<div class="form-actions">
+			<input name="submit" type="submit" id="submit" tabindex="5" value="Submit Comment" 
+				class="btn btn-primary" />
+			<?php comment_id_fields(); ?>
+		</div>
+		
+		<?php do_action('comment_form', $post->ID); ?>
  
-<p><textarea name="comment" id="comment" cols="100%" rows="10" tabindex="4"></textarea></p>
- 
-<p><input name="submit" type="submit" id="submit" tabindex="5" value="Submit Comment" />
-<?php comment_id_fields(); ?>
-</p>
-<?php do_action('comment_form', $post->ID); ?>
- 
-</form>
- 
-<?php endif; // If registration required and not logged in ?>
+	</form>
+<?php endif;?>
 </div>
- 
-<?php endif; // if you delete this the sky will fall on your head ?>
+<?php endif;?>
