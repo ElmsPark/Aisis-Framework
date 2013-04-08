3acb0daee5e80bf0417218f4f9fe7e9f653066e9
adam <a-kyle@gotmail.com>
2013-04-08 17:42:29 -0600

    Cleaned up, fixed and changes some layout jazz.

--- CoreTheme/ShortCodes/assets/js/ShortCode.js
+++ CoreTheme/ShortCodes/assets/js/ShortCode.js
@@ -3,7 +3,7 @@ function button() {
 
 	try{
 		var win = window.dialogArguments || opener || parent || top;
-		win.send_to_editor('[button size="" color="" link="" title=""]');
+		win.send_to_editor('[button size="" color="" link="" font_icon="" title=""]');
 	}catch(e){
 		console.log("Could not send to window: " + e);
 	}
