441d532323a80590a4702e4b1e064bcf708fcddf
Adam <a-kyle@hotmail.com>
2013-04-29 17:54:49 -0600

    Narrow page Template Added.
    Removed newly added short codes, replaced with a template instead. There is a new page template called "Narrow" which allows for narrow page content.

---
+++ narrow-page-template.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Template Name: narrow-page
+*/
+$option = array(
+	'title_header' => 'h2',
+	'page' => array(
+		'content' => array(
+			'before' => '<div class="post">',
+			'after' => '</div>'
+		),			
+		'image' => array(
+			'size' => 'full',
+			'args' => array(
+				'align' => 'center',
+				'class' => 'marginBottom20 marginTop20 thumbnail'
+			)
+		),
+	),
+);
+
+get_header();
+echo '<div class="container-narrow marginTop60">';
+	$loop = new CoreTheme_Template_Helpers_CustomLoop($option);
+	$loop->loop();
+echo '</div>';
+get_footer();
